24ed6ed33d8eb8151ebca1974d32675eb8c2fffe
Stepanyan Eduard <stepanyan_e@inbox.ru>
2020-05-16 16:53:09 +0300

    Initial commit

---
+++ src/test/java/com/steppedua/jobhunters/JobHuntersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steppedua.jobhunters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHuntersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
