6b8b5360d2b58283a03441fc51593aa3d2facd6d
Stepanyan Eduard <stepanyan_e@inbox.ru>
2020-05-17 21:28:22 +0300

    adding static pages of the website

---
+++ src/main/java/com/steppedua/jobhunters/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.steppedua.jobhunters.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+}
